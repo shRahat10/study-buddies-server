6070852b27e29b956c1d338914203b04f0c0b933
shRahat10 <shahriar.haque.1011@gmail.com>
2024-05-12 15:51:07 +0600

    submission

--- index.js
+++ index.js
@@ -116,6 +116,12 @@ async function run() {
       res.send(result);
     })
 
+    app.post('/submissions', async (req, res) => {
+      const newSubmissions = req.body;
+      const result = await submissions.insertOne(newSubmissions);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
